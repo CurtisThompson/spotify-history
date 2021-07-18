f2e7ddbb9840b231cf04b4211af85c8454ba4083
Curtis Thompson <curtiswaynethompson@gmail.com>
2021-07-18 14:37:50 +0100

    Update statistics.py
    Statistics added that use the API

--- src/statistics.py
+++ src/statistics.py
@@ -35,6 +35,129 @@ client_credentials_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_I
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
+def get_song_artists_through_api(artist, track, sp=sp):
+    """Get all song artist details for a song using the Spotify API."""
+    search_query = artist + ' ' + track
+    song_details = sp.search(search_query, limit=1)
+    song_artists = song_details['tracks']['items'][0]['artists']
+    return song_artists
+
+
+def get_songs_artists_frame(songs_df, sp=sp):
+    """Gets all possible artists for all possible songs in a DataFrame."""
+    all_songs_with_artists = []
+
+    for index, row in songs_df.iterrows():
+        try:
+            # Get song artists
+            song_artists = get_song_artists_through_api(row.artistName,
+                                                        row.trackName,
+                                                        sp=sp)
+            
+            # Store artists for song
+            uris = [artist['uri'] for artist in song_artists]
+            for uri in uris:
+                all_songs_with_artists.append([row.artistName,
+                                               row.trackName,
+                                               uri])
+        except:
+            if ' - ' in row.trackName:
+                try:
+                    # Get song artists
+                    new_track_name = ' - '.join(row.trackName.split(' - ')[:-1])
+                    song_artists = get_song_artists_through_api(row.artistName,
+                                                                new_track_name,
+                                                                sp=sp)
+                    
+                    # Store artists for song
+                    uris = [artist['uri'] for artist in song_artists]
+                    for uri in uris:
+                        all_songs_with_artists.append([row.artistName,
+                                                       row.trackName,
+                                                       uri])
+                except:
+                    print(index, row.artistName, row.trackName)
+            else:
+                print(index, row.artistName, row.trackName)
+    
+    # Convert results to dataframe
+    all_songs_with_artists = pd.DataFrame(all_songs_with_artists,
+                                          columns=['artistName',
+                                                   'trackName',
+                                                   'artistURI'])
+    
+    # Get a list of URIs
+    artist_uris = list(all_songs_with_artists.artistURI.unique())
+    
+    return all_songs_with_artists, artist_uris
+
+
+def get_artists_from_uris(artist_uris, sp=sp):
+    """Convert a list of artist URIs to data dicts with the Spotify API."""
+    artist_search = {}
+    for i in range(0, len(artist_uris), 50):
+        search_results = sp.artists(artist_uris[i:i + 50])
+        for result in search_results['artists']:
+            artist_search[result['uri']] = result
+    return artist_search
+
+
+def check_uri_for_song(uri, trackName, artistName):
+    """Does an artist URI feature in a given song."""
+    song_filter = (all_songs_with_artists.artistName==artistName) & (all_songs_with_artists.trackName==trackName)
+    return uri in all_songs_with_artists[song_filter].artistURI.unique()
+
+
+def get_artists_by_play_time(data, artist_search):
+    """Count play time for each artist."""
+    # Get play time for each artist
+    df_artists = data[['trackName', 'artistName', 'msPlayed']]
+    artist_play_times = []
+    for uri in list(artist_search.keys()):
+        artist_play_time = df_artists[df_artists.apply(lambda x: check_uri_for_song(uri, x.trackName, x.artistName), axis=1)].msPlayed.sum()
+        artist_play_times.append((uri, artist_play_time))
+
+    # Get artist names and time
+    top_artists = pd.DataFrame(artist_play_times, columns=['URI', 'Ms'])
+    top_artists['Name'] = top_artists.URI.apply(lambda x: artist_search[x]['name'])
+    top_artists['Minutes'] = top_artists.Ms / 60000
+    return top_artists
+
+
+def get_genres_by_play_time(data, artist_search, all_songs_with_artists):
+    """Count play time for each genre."""
+    # Get a list of genres
+    genres = []
+    for artist in artist_search:
+        genres.extend(artist_search[artist]['genres'])
+    genres = list(set(genres))
+    
+    # Set up genre time dict
+    genre_times = {}
+    for genre in genres:
+        genre_times[genre] = 0
+    
+    # Get genre for each song
+    for index, row in data.iterrows():
+        # Get main artist
+        song_data = all_songs_with_artists[(all_songs_with_artists.trackName==row.trackName) & (all_songs_with_artists.artistName==row.artistName)]
+        song_data = song_data.copy().reset_index()
+        try:
+            main_artist = song_data.iloc[0].artistURI
+        except:
+            main_artist = None
+        
+        # Add genres of main artist to counter
+        if main_artist != None:
+            artist_genres = artist_search[main_artist]['genres']
+            for genre in artist_genres:
+                genre_times[genre] += row.msPlayed
+    
+    # Return as DataFrame
+    df_genre = pd.DataFrame(list(genre_times.items()), columns=['Genre', 'Ms'])
+    df_genre['Minutes'] = df_genre.Ms / 60000
+    return df_genre
+
 
 def get_songs_by_listens(data):
     """Returns a count for each song, ordered by most listens."""
